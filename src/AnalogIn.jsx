a92b99c9fc5bb24e646c09814f41db710abb8704
alfu32 <ioan.alferaru@gmail.com>
2022-06-24 12:00:31 +0200

    1 file modified

--- src/AnalogIn.jsx
+++ src/AnalogIn.jsx
@@ -4,7 +4,6 @@ import { humanReadout } from './human-numbers';
 
 function useRandomReading({ range }) {
   const [analogIn, setAnalogIn] = useState(0);
-
   setTimeout(() => {
     setAnalogIn(Math.random() * 20000 - 10000);
   }, 1000);
@@ -18,12 +17,21 @@ function useRandomReading({ range }) {
 }
 
 function AnalogIn({ id }) {
-  const [reading] = useRandomReading();
+  const [analogIn, setAnalogIn] = useState(0);
+  setTimeout(() => {
+    setAnalogIn(Math.random() * 20000 - 10000);
+  }, 1000);
+  useEffect(() => {
+    // Update count to be 5 after timeout is scheduled
+    setTimeout(() => {
+      setAnalogIn(Math.random() * 20000 - 10000);
+    }, 1000);
+  }, []);
   return (
     <div className="AnalogIn">
       <h4 style={{ display: 'inline-block' }}>Analog In({id})</h4>
-      <input type="range" min="0" max="3.3" step="0.001" value={pinValue} />
-      {humanReadout({ value: reading, unit: 'V' })}
+      <input type="range" min="0" max="3.3" step="0.001" value={analogIn} />
+      {humanReadout({ value: analogIn, unit: 'V' })}
     </div>
   );
 }
